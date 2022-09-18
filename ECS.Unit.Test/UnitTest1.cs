83e3347d02c5df3cc33b1511f8d3d94570b59cd0
Moheb420 <68193758+Moheb420@users.noreply.github.com>
2022-09-18 16:24:00 +0200

    Unit test deleted

---
+++ ECS.Unit.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace ECS.Unit.Test
+using System.Data
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
