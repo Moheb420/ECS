0bbfc57108b7e5b69e6eb2e2d765aaa1d824cbc7
Moheb420 <68193758+Moheb420@users.noreply.github.com>
2022-09-28 18:28:11 +0200

    ECS tested with coverage

---
+++ ECS.Legacy/FakeHeater.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECS.Legacy
+{
+    public class FakeHeater:IFakeHeater
+    {
+       
+
+        bool IFakeHeater.FakeTurnOn(int i)
+        {
+            if (i == 1)
+            {
+                return true;
+            }
+            else if (i == 2)
+            {
+                return false;
+            }
+
+            return false;
+
+        }
+    }
+}
---
+++ ECS.Legacy/IFakeHeater.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECS.Legacy
+{
+    public interface IFakeHeater
+    {
+        bool FakeTurnOn(int i);
+    }
+}
