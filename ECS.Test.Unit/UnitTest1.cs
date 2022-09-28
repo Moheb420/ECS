0f6101ea6338b7dc5f0a022c10eb959d14be971a
Moheb420 <68193758+Moheb420@users.noreply.github.com>
2022-09-28 18:49:32 +0200

    Test added

---
+++ ECS.Test.Unit/UnitTest1.cs
@@ -0,0 +1,26 @@
+using ECS.Legacy;
+
+namespace ECS.Unit.Test
+{
+    public class Tests
+    {
+
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+        [Test]
+        public void Test1()
+        {
+            IFakeHeater fakeHeater = new FakeHeater();
+            bool variable = fakeHeater.FakeTurnOn(1);
+            bool test2 = fakeHeater.FakeTurnOn(2);
+            bool test3 = fakeHeater.FakeTurnOn(3);
+            Assert.That(true, Is.EqualTo(variable));
+            Assert.That(false, Is.EqualTo(test2));
+            Assert.That(false, Is.EqualTo(test3));
+        }
+    }
+}
